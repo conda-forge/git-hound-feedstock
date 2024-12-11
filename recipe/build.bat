cc9e812ea5b96cf9ae190cf45262ac0859949ad3
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-11 00:27:51 -0800

    add pcre

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,3 +1,4 @@
+go get github.com/GRbit/go-pcre || goto :error
 go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
 go-licenses save . --save_path=license-files || goto :error
 
